8df9fa96296d1470476729e1a61361a12cc97cc0
Suman Bhattarai <sumanbhattaraiofficial@gmail.com>
2022-03-17 10:39:39 +0545

    Fix picker ui

--- src/components/Picker/index.tsx
+++ src/components/Picker/index.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/Feather';
 
-import Fonts from 'utils/Fonts';
 import styles from './styles';
+import Fonts from 'utils/Fonts';
+import Colors from 'utils/Colors';
+import {wp} from 'utils/Constants';
 
 interface Props {
   items: Array<{label: string; value: string}>;
@@ -22,8 +25,11 @@ const Picker = ({items}: Props) => {
       setValue={setValue}
       style={styles.style}
       dropDownContainerStyle={styles.dropDownContainer}
-      labelStyle={{fontFamily: Fonts.regular}}
-      textStyle={{fontFamily: Fonts.regular}}
+      labelStyle={styles.label}
+      textStyle={styles.label}
+      TickIconComponent={() => (
+        <Icon name="check" size={wp(4)} color={Colors.primary} />
+      )}
     />
   );
 };
--- src/components/Picker/styles.ts
+++ src/components/Picker/styles.ts
@@ -1,11 +1,13 @@
 import {StyleSheet} from 'react-native';
 
 import Colors from 'utils/Colors';
+import Fonts from 'utils/Fonts';
 
 const styles = StyleSheet.create({
-  style: {borderColor: Colors.gray, borderRadius: 0, marginTop: 16},
+  style: {borderWidth: 0, borderRadius: 0, marginTop: 16},
   dropDown: {fontSize: 90},
-  dropDownContainer: {borderRadius: 0},
+  dropDownContainer: {borderRadius: 0, borderColor: Colors.lightGray},
+  label: {fontFamily: Fonts.regular},
 });
 
 export default styles;
